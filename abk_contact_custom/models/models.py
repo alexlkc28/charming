0405da84460fcdf2723378f97d89d0e8cf245d6b
Bill Nguyen <bill@up5tech.com>
2022-05-30 23:07:40 +0700

    Add English Name and English Address for Contact module

--- abk_contact_custom/models/models.py
+++ abk_contact_custom/models/models.py
@@ -8,7 +8,7 @@ class custom_res_partner(models.Model):
     name_chinese = fields.Char('Chinese Name')
     name_english = fields.Char('English Name')
 
-    # Address fields
+    # Chinese Address Fields
     chinese_street = fields.Char('Street', readonly=False, store=True)
     chinese_street2 = fields.Char('Street2', readonly=False, store=True)
     chinese_zip = fields.Char('Zip', change_default=True, readonly=False, store=True)
@@ -17,4 +17,15 @@ class custom_res_partner(models.Model):
         "res.country.state", string='State', readonly=False, store=True,
         domain="[('country_id', '=?', country_id)]")
     chinese_country_id = fields.Many2one(
+        'res.country', string='Country', readonly=False, store=True)
+
+    # English Address Fields
+    english_street = fields.Char('Street', readonly=False, store=True)
+    english_street2 = fields.Char('Street2', readonly=False, store=True)
+    english_zip = fields.Char('Zip', change_default=True, readonly=False, store=True)
+    english_city = fields.Char('City', readonly=False, store=True)
+    english_state_id = fields.Many2one(
+        "res.country.state", string='State', readonly=False, store=True,
+        domain="[('country_id', '=?', country_id)]")
+    english_country_id = fields.Many2one(
         'res.country', string='Country', readonly=False, store=True)
