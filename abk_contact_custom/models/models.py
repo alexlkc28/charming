d306db7d2c6b657cbf90b76689accae2f225286f
Peter Nguyen <haonguyenhcmus@gmail.com>
2022-05-31 11:38:40 +0700

    add fields in module Contact

--- abk_contact_custom/models/models.py
+++ abk_contact_custom/models/models.py
@@ -6,14 +6,20 @@ from odoo import models, fields, api
 class custom_res_partner(models.Model):
     _inherit = 'res.partner'
     name_chinese = fields.Char('Chinese Name')
+    name_english = fields.Char('English Name')
+    address_chinese = fields.Text('Chinese Address')
+    address_english = fields.Text('English Address')
+    fax = fields.Char('Fax')
+    attention = fields.Char('Attention')
+    invattn = fields.Char('Invoice Attention')
+    contact = fields.Char('Contact')
+    lang_use = fields.Selection([('english', 'E'), ('chinese', 'C')], string="Language")
+    payterm = fields.Selection([('option_1', '0'), ('option_2', '7'),
+                                ('option_3', '15'), ('option_4', '20'),
+                                ('option_5', '30'), ('option_6', '45'),
+                                ('option_7', '60'), ('option_8', '90')], string="Payterm")
 
-    # Address fields
-    chinese_street = fields.Char('Street', readonly=False, store=True)
-    chinese_street2 = fields.Char('Street2', readonly=False, store=True)
-    chinese_zip = fields.Char('Zip', change_default=True, readonly=False, store=True)
-    chinese_city = fields.Char('City', readonly=False, store=True)
-    chinese_state_id = fields.Many2one(
-        "res.country.state", string='State', readonly=False, store=True,
-        domain="[('country_id', '=?', country_id)]")
-    chinese_country_id = fields.Many2one(
-        'res.country', string='Country', readonly=False, store=True)
+
+
+
+
