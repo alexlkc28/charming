d89cbf3a9cf05b629ba7297ec3552bf28a32454f
Peter Nguyen <haonguyenhcmus@gmail.com>
2022-05-31 16:02:02 +0700

    update fields in module Contact

--- abk_contact_custom/models/models.py
+++ abk_contact_custom/models/models.py
@@ -5,6 +5,7 @@ from odoo import models, fields, api
 
 class custom_res_partner(models.Model):
     _inherit = 'res.partner'
+    customer_no = fields.Char('Customer Number')
     name_chinese = fields.Char('Chinese Name')
     name_english = fields.Char('English Name')
     address_chinese = fields.Text('Chinese Address')
@@ -13,6 +14,10 @@ class custom_res_partner(models.Model):
     attention = fields.Char('Attention')
     invattn = fields.Char('Invoice Attention')
     contact = fields.Char('Contact')
+    paydesc = fields.Text('Payment Description')
+    remark = fields.Text('Remark')
+    monthpay = fields.Char('Month pay')
+    currency = fields.Many2one("res.currency", string="Currency")
     lang_use = fields.Selection([('english', 'E'), ('chinese', 'C')], string="Language")
     payterm = fields.Selection([('option_1', '0'), ('option_2', '7'),
                                 ('option_3', '15'), ('option_4', '20'),
