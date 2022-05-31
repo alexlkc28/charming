dd0c3b7065d81b3809caf5e57c5d6897fd12efde
Peter Nguyen <haonguyenhcmus@gmail.com>
2022-05-31 11:40:04 +0700

    add hide fields in module Contact

--- abk_contact_custom/models/models.py
+++ abk_contact_custom/models/models.py
@@ -5,19 +5,19 @@ from odoo import models, fields, api
 
 class custom_res_partner(models.Model):
     _inherit = 'res.partner'
-    name_chinese = fields.Char('Chinese Name')
-    name_english = fields.Char('English Name')
-    address_chinese = fields.Text('Chinese Address')
-    address_english = fields.Text('English Address')
-    fax = fields.Char('Fax')
-    attention = fields.Char('Attention')
-    invattn = fields.Char('Invoice Attention')
-    contact = fields.Char('Contact')
-    lang_use = fields.Selection([('english', 'E'), ('chinese', 'C')], string="Language")
-    payterm = fields.Selection([('option_1', '0'), ('option_2', '7'),
-                                ('option_3', '15'), ('option_4', '20'),
-                                ('option_5', '30'), ('option_6', '45'),
-                                ('option_7', '60'), ('option_8', '90')], string="Payterm")
+    # name_chinese = fields.Char('Chinese Name')
+    # name_english = fields.Char('English Name')
+    # address_chinese = fields.Text('Chinese Address')
+    # address_english = fields.Text('English Address')
+    # fax = fields.Char('Fax')
+    # attention = fields.Char('Attention')
+    # invattn = fields.Char('Invoice Attention')
+    # contact = fields.Char('Contact')
+    # lang_use = fields.Selection([('english', 'E'), ('chinese', 'C')], string="Language")
+    # payterm = fields.Selection([('option_1', '0'), ('option_2', '7'),
+    #                             ('option_3', '15'), ('option_4', '20'),
+    #                             ('option_5', '30'), ('option_6', '45'),
+    #                             ('option_7', '60'), ('option_8', '90')], string="Payterm")
 
 
 
