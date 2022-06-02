f98f7a3e839d1ed6cb21ee744f1cbe263872b130
Peter Nguyen <haonguyenhcmus@gmail.com>
2022-06-02 16:31:12 +0700

    update Module Sale Order Contact Module

--- abk_oder_custom/models/models.py
+++ abk_oder_custom/models/models.py
@@ -5,9 +5,9 @@ from odoo import models, fields, api
 
 class custom_sale_order(models.Model):
     _inherit = 'sale.order'
-    # paid = fields.Char('Paid')
-    # remaining = fields.Char('Remaining')
-    # payment_schedule = fields.Char('Payment Schedule')
+    paid = fields.Char('Paid')
+    remaining = fields.Char('Remaining')
+    payment_schedule = fields.Char('Payment Schedule')
 
 
 
