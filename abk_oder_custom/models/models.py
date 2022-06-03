6281d87246a6d14551137140d9b5fe818fe3e344
Peter Nguyen <haonguyenhcmus@gmail.com>
2022-06-03 11:06:43 +0700

    hide field in Order Module

--- abk_oder_custom/models/models.py
+++ abk_oder_custom/models/models.py
@@ -5,9 +5,10 @@ from odoo import models, fields, api
 
 class custom_sale_order(models.Model):
     _inherit = 'sale.order'
-    paid = fields.Char('Paid')
-    remaining = fields.Char('Remaining')
-    payment_schedule = fields.Char('Payment Schedule')
+    # paid = fields.Integer('Paid')
+    # remaining = fields.Integer('Remaining')
+    # payment_schedule = fields.Selection([('late', 'Late'), ('not_late', 'Not late')], string="Payment Schedule")
+    # can_deliver = fields.Selection([('yes', 'Yes'), ('no', 'No')], string="Can deliver")
 
 
 
